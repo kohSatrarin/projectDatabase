cd60e28da127ddbb8f33b6b0ea93c370dddd12a0
satrarin <koh.satrarin@gmail.com>
2018-11-02 01:17:41 +0700

    can add new customer

--- main.js
+++ main.js
@@ -10,6 +10,15 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
+  console.log("Connected!");
+  //test add new customer
+  var addNewCostomer =
+    "INSERT INTO `customer` (`id`, `fname`, `lname`, `dob`, `gender`, `phone`, `home_addr`, `work_addr`, `account_id`) VALUES ('', 'ccccc', 'dddddd', '2018-10-11', 'F', '02000000001', 'home', 'work', '000000002');";
+  con.query(addNewCostomer, function(err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+  //text pull all customer
   con.query("SELECT * FROM `customer` WHERE 1", function(err, result, fields) {
     if (err) throw err;
     console.log(result);
