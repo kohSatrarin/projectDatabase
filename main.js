d6ab0eba5811f7b1ff2eb1325b8edd5b391ff268
satrarin <koh.satrarin@gmail.com>
2018-11-07 14:24:33 +0700

    separate function

--- main.js
+++ main.js
@@ -8,31 +8,40 @@ var con = mysql.createConnection({
   database: "mydb"
 });
 
+// function hello(name) {
+//   console.log("hello " + name);
+// }
+
+function addNewCustomer() {
+  var addNewCustomer =
+    "INSERT INTO `customer` (`id`, `fname`,`lname`, `dob`, `gender`, `phone`, `home_addr`, `work_addr`, `account_id`) VALUES ('04', 'Mr.A', 'B', '2018-11-01', 'M', '01234567890', 'cmu chiangMai', 'cmu chiangMai', 'acountMrA');";
+  con.query(addNewCustomer, function(err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+}
+
+function showAllUser() {
+  con.query("SELECT * FROM `customer` WHERE 1", function(err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+}
+
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
   //need function generate id for custumer
-  con.query("INSERT INTO posts SET ?", { title: "test" }, function(
-    error,
-    results,
-    fields
-  ) {
-    if (error) throw error;
-    console.log(results.insertId);
-  });
 
   //test add new customer
-  var addNewCustomer =
-    "INSERT INTO `customer` (`id`, `fname`, `lname`, `dob`, `gender`, `phone`, `home_addr`, `work_addr`, `account_id`) VALUES ('', 'Mr.A', 'B', '2018-11-01', 'M', '01234567890', 'cmu chiangMai', 'cmu chiangMai', 'acountMrA');";
-  con.query(addNewCostomer, function(err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
+  //addNewCustomer();
 
   //text pull all customer
-  con.query("SELECT * FROM `customer` WHERE 1", function(err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
+  // showAllUser();
+
+  // hello();
+  // hello("CSS", "HTML", "AAA", 4);
 });
+
+con.end(); //close connection to mysql
